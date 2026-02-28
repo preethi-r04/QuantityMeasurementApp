e7dbb05ec34d46eebba45dd4a2d1d9e883b2235f
preethi-r04 <preethiliki2004@gmail.com>
2026-02-28 11:34:21 +0530

    [Preethi]: Added: UC6: implement UC6-addition of length units

--- src/test/java/com.bridgelabz/QuantityLengthTest.java
+++ src/test/java/com.bridgelabz/QuantityLengthTest.java
@@ -6,25 +6,60 @@ import static org.junit.jupiter.api.Assertions.*;
 class QuantityLengthTest {
 
     @Test
+    void testAddition_SameUnit_FeetPlusFeet() {
+
+        QuantityLength a =
+                new QuantityLength(1, LengthUnit.FEET);
+        QuantityLength b =
+                new QuantityLength(2, LengthUnit.FEET);
+
+        assertEquals(3,
+                a.add(b).getValue(), 0.0001);
+    }
+
+    @Test
+    void testAddition_CrossUnit_FeetPlusInches() {
+
+        QuantityLength a =
+                new QuantityLength(1, LengthUnit.FEET);
+        QuantityLength b =
+                new QuantityLength(12, LengthUnit.INCHES);
+
+        assertEquals(2,
+                a.add(b).getValue(), 0.0001);
+    }
+
+    @Test
     void testConversion_FeetToInches() {
-        assertEquals(12.0,
-                QuantityLength.convert(1.0,
+
+        double result =
+                QuantityLength.convert(
+                        1.0,
                         LengthUnit.FEET,
-                        LengthUnit.INCHES), 0.0001);
+                        LengthUnit.INCHES);
+
+        assertEquals(12, result, 0.0001);
     }
 
     @Test
-    void testConversion_YardsToFeet() {
-        assertEquals(9.0,
-                QuantityLength.convert(3.0,
-                        LengthUnit.YARDS,
-                        LengthUnit.FEET), 0.0001);
+    void testAddition_WithZero() {
+
+        QuantityLength a =
+                new QuantityLength(5, LengthUnit.FEET);
+        QuantityLength b =
+                new QuantityLength(0, LengthUnit.INCHES);
+
+        assertEquals(5,
+                a.add(b).getValue(), 0.0001);
     }
 
     @Test
-    void testConversion_InvalidUnit() {
+    void testAddition_Null() {
+
+        QuantityLength a =
+                new QuantityLength(1, LengthUnit.FEET);
+
         assertThrows(IllegalArgumentException.class,
-                () -> QuantityLength.convert(
-                        1.0, null, LengthUnit.FEET));
+                () -> a.add(null));
     }
 }
